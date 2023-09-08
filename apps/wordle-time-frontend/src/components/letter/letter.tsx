f96e2871a8150974c8749336e9becccacfa53acd
Julian Turner <julian@trner.de>
2023-09-08 11:00:18 +0000

    add game page

--- apps/wordle-time-frontend/src/components/letter/letter.tsx
+++ apps/wordle-time-frontend/src/components/letter/letter.tsx
@@ -4,26 +4,26 @@ import { LetterState } from "../../routes/game"
 export interface LetterProps {
   letter: string,
   index: number,
-  letterState: LetterState
+  letterState: string
   onLetterChange: QRL<(index: number, letter: string) => void>;
 }
 
 export default component$<LetterProps>((props) => {
 
   return (
-
-    <input type="text" class={
-      ['w-8', 'h-8', 'md:w-12', 'md:h-12', 'lg:w-24', 'lg:h-24', 'text-center', 'mx-2', 'text-ctp-text', 'border', 'border-4', 'rounded-lg', 'bg-ctp-mantle', 'font-bold', 'uppercase', 'text-l', 'md:text-lg', 'lg:text-6xl',
-        { "border-ctp-green": props.letterState == 0 },
-        { "border-ctp-yellow": props.letterState == 1 },
-        { "border-ctp-red": props.letterState == 2 },
-        { "border-ctp-mantle": props.letterState == 3 },]
-    }
-      value={props.letter} maxLength={1} onInput$={
-        (evt) => {
-          props.onLetterChange(props.index, (evt.target as HTMLInputElement).value)
-        }
-      } />
-
+    <>
+      <input type="text" class={
+        ['w-8', 'h-8', 'md:w-12', 'md:h-12', 'lg:w-24', 'lg:h-24', 'text-center', 'mx-2', 'text-ctp-text', 'border', 'border-4', 'rounded-lg', 'bg-ctp-mantle', 'font-bold', 'uppercase', 'text-l', 'md:text-lg', 'lg:text-6xl', 'hover:scale-125',
+          { "border-ctp-green": props.letterState == "CorrectSpot" },
+          { "border-ctp-yellow": props.letterState == "WrongSpot" },
+          { "border-ctp-red": props.letterState == "WrongLetter" },
+          { "border-ctp-sky": props.letterState == "Empty" },]
+      }
+        value={props.letter} maxLength={1} onInput$={
+          (evt) => {
+            props.onLetterChange(props.index, (evt.target as HTMLInputElement).value)
+          }
+        } />
+    </>
   )
 })
--- apps/wordle-time-frontend/src/routes/game/index.tsx
+++ apps/wordle-time-frontend/src/routes/game/index.tsx
@@ -1,15 +1,13 @@
-import { $, component$, useSignal, useStore } from "@builder.io/qwik";
-import { Form, RequestHandler } from "@builder.io/qwik-city";
-import { Input } from "postcss";
+import { $, component$, useStore } from "@builder.io/qwik";
 import Letter from "../../components/letter/letter";
+import { server$ } from "@builder.io/qwik-city";
 
 export enum LetterState {
-  "CorrectSpot" = 0,
-  "WrongSpot" = 1,
-  "WrongLetter" = 2,
-  "Empty" = 3
+  "CorrectSpot",
+  "WrongSpot",
+  "WrongLetter",
+  "Empty"
 }
-
 
 export interface GuessResult {
   letterStates: LetterState[];
@@ -19,10 +17,20 @@ export interface CurrentGuess {
   letter: string[];
 }
 
+const endpoint = "http://localhost:8090/api/guess?word=";
+
+const serverResponse = server$(async (guess: CurrentGuess) => {
+  const response = await fetch(endpoint + guess.letter.join("").toLowerCase());
+  const json = await response.json();
+  console.log(json);
+  return json;
+});
+
 export default component$(() => {
+
   const store = useStore(
     {
-      tries: 0,
+      tries: 5,
       isComplete: false,
       CurrentGuess: {
         letter: ["a", "b", "a", "a", "b"]
@@ -33,30 +41,66 @@ export default component$(() => {
     }
   )
 
+
+
   const onLetterChange = $((index: number, letter: string) => {
     store.CurrentGuess.letter[index] = letter;
   });
-
 
   return (
     <>
       <div>Game</div>
       <section class="p-4">
-        <div> Tries: {store.tries}</div>
+        <div> Tries: {store.tries} / 6</div>
         <div> Current Guess: {store.CurrentGuess.letter.join("")}</div>
         <div> Guess Result: {store.GuessResult.letterStates.join("")}</div>
-        {store.CurrentGuess.letter.map((letter, index) => (
-          <Letter
-            key={index}
-            index={index}
-            letter={letter}
-            letterState={store.GuessResult.letterStates[index]}
-            onLetterChange={onLetterChange} />
-        ))}
       </section>
+
+      <div> Guess Result: {store.GuessResult.letterStates.join("")}</div>
+      {
+        !store.isComplete && store.tries < 6 && (
+          <>
+            <div class="flex items-center justify-center">
+              {store.CurrentGuess.letter.map((letter, index) => (
+                <Letter
+                  key={index}
+                  index={index}
+                  letter={letter}
+                  letterState={store.GuessResult.letterStates[index]}
+                  onLetterChange={onLetterChange} />
+              ))}
+            </div>
+            <div class="flex items-center justify-center my-16">
+              <button class="rounded-lg border-4 p-2 px-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base"
+                onClick$={async () => {
+                  const response = await serverResponse(store.CurrentGuess);
+                  store.GuessResult.letterStates = response.letterStates;
+                }}
+
+
+              >Raten</button>
+            </div>
+          </>
+        )
+      }
+      {
+        store.isComplete && (
+          <div class="flex items-center justify-center my-16">
+            <h1>You made it</h1>
+          </div>
+        )
+      }
+      {
+        store.tries >= 6 && !store.isComplete && (
+          <div class="flex items-center justify-center my-16">
+            <h1>You lost</h1>
+          </div>
+        )
+      }
     </>
-  )
-});
+  );
+}
+);
 
 
 
