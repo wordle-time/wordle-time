646c06e7b56cf1414f32f72d5a791236d6ab9f6c
Julian Turner <julian@trner.de>
2023-09-11 17:31:12 +0000

    Updated Model
    Co-authored-by: Kay Fischbach <E3FxGaming@users.noreply.github.com>

--- apps/wordle-time-frontend/src/routes/requirements/index.tsx
+++ apps/wordle-time-frontend/src/routes/requirements/index.tsx
@@ -5,11 +5,23 @@ import fetch from "node-fetch";
 export interface Requirement {
   id: string
   title: string
-  referece: string
+  reference: string
   description: string
   impact: string
   criteria: string[]
+  testCases: TestCase[]
+  actPic: string
+  seqPic: string
 }
+
+export interface TestCase {
+  name: string
+  requirement: string
+  action: string
+  expectation: string
+  testPic: string
+}
+
 
 
 export interface Requirements {
