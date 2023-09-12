fcacd19f7cfce26eeee4e81c6fdc3ed2e8cb18dc
Julian Turner <julian@trner.de>
2023-09-12 18:49:01 +0000

    add localstorage

--- apps/wordle-time-frontend/src/routes/game/index.tsx
+++ apps/wordle-time-frontend/src/routes/game/index.tsx
@@ -1,6 +1,7 @@
-import { $, component$, useStore } from "@builder.io/qwik";
+import { $, component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
 import Letter from "../../components/letter/letter";
-import { routeAction$, server$ } from "@builder.io/qwik-city";
+import { server$ } from "@builder.io/qwik-city";
+import { animate } from "motion";
 
 export enum LetterState {
   "Undefiend" = 0,
@@ -34,13 +35,15 @@ export interface GameState {
   GuessResult: GuessResult;
 }
 
+
 export default component$(() => {
+
 
   const store = useStore<GameState>(
     {
       tryCount: 0,
       isComplete: false,
-      isLoading: false,
+      isLoading: true,
       CurrentGuess: {
         letter: ["A", "E", "I", "O", "U"]
       },
@@ -56,59 +59,84 @@ export default component$(() => {
     }
   )
 
+  // load the state of the game from local storage
+  useVisibleTask$(() => {
+    const localStorage = Object.keys(window.localStorage).find(key => key === "gameState");
+    if (localStorage) {
+      const gameState = JSON.parse(window.localStorage.getItem("gameState") || "");
+      store.tryCount = gameState.tryCount;
+      store.isComplete = gameState.isComplete;
+      store.isLoading = gameState.isLoading;
+      store.CurrentGuess = gameState.CurrentGuess;
+      store.GuessResult = gameState.GuessResult;
+      console.log(gameState);
+    } else {
+      console.log("no local storage");
+    }
+  });
+
 
 
   const onLetterChange = $((index: number, letter: string) => {
     store.CurrentGuess.letter[index] = letter;
   });
 
+
+
   return (
-    <>
-      {
-        !store.isComplete && store.tryCount < 6 && (
-          <>
-            <div class="flex items-center justify-center">
-              {store.CurrentGuess.letter.map((letter, index) => (
-                <Letter
-                  key={index}
-                  index={index}
-                  letter={letter}
-                  letterState={store.GuessResult.letterStates[index]}
-                  onLetterChange={onLetterChange} />
-              ))}
-            </div>
+    <div class='grid h-screen place-items-center'>
+      <div>
+        {
+          !store.isComplete && store.tryCount < 6 && (
+            <>
+              <div class="flex items-center justify-center">
+                {store.CurrentGuess.letter.map((letter, index) => (
+                  <Letter
+                    key={index}
+                    index={index}
+                    letter={letter}
+                    letterState={store.GuessResult.letterStates[index]}
+                    onLetterChange={onLetterChange} />
+                ))}
+              </div>
+              <div class="flex items-center justify-center my-16">
+                <button disabled={
+                  store.CurrentGuess.letter.join("").length < 5
+                } class="rounded-lg disabled:hover:cursor-not-allowed disabled:border-ctp-red disabled:bg-ctp-red disabled:text-ctp-crust border-4 p-2 px-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base"
+                  onClick$={async () => {
+                    const response = await serverResponse(store.CurrentGuess);
+                    store.GuessResult.letterStates = response.letterStates;
+                    store.tryCount = store.tryCount + 1;
+                    animate(".tryCount", {
+                      scale: [1, 1.5, 1],
+
+                    })
+                    window.localStorage.setItem("gameState", JSON.stringify(store));
+                  }}
+                >Raten</button>
+              </div>
+              <div class="flex items-center justify-center my-16">
+                <h3 class="text-3xl tryCount" > Versuche: {store.tryCount} / 6</h3>
+              </div>
+            </>
+          )
+        }
+        {
+          store.isComplete && (
             <div class="flex items-center justify-center my-16">
-              <button disabled={
-                store.CurrentGuess.letter.join("").length < 5
-              } class="rounded-lg disabled:hover:cursor-not-allowed disabled:border-ctp-red disabled:bg-ctp-red disabled:text-ctp-crust border-4 p-2 px-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base"
-                onClick$={async () => {
-                  const response = await serverResponse(store.CurrentGuess);
-                  store.GuessResult.letterStates = response.letterStates;
-                  store.tryCount = store.tryCount + 1;
-                }}
-              >Raten</button>
+              <h1 class="text-3xl text-ctp-green">You made it</h1>
             </div>
+          )
+        }
+        {
+          store.tryCount >= 6 && !store.isComplete && (
             <div class="flex items-center justify-center my-16">
-              <h3 class="text-3xl"> Versuche: {store.tryCount} / 6</h3>
+              <h1 class="text-3xl text-ctp-red">You lost</h1>
             </div>
-          </>
-        )
-      }
-      {
-        store.isComplete && (
-          <div class="flex items-center justify-center my-16">
-            <h1 class="text-3xl text-ctp-green">You made it</h1>
-          </div>
-        )
-      }
-      {
-        store.tryCount >= 6 && !store.isComplete && (
-          <div class="flex items-center justify-center my-16">
-            <h1 class="text-3xl text-ctp-red">You lost</h1>
-          </div>
-        )
-      }
-    </>
+          )
+        }
+      </div>
+    </div>
   );
 }
 );
@@ -163,4 +191,8 @@ export default component$(() => {
 
 
 
+
+function useClientEffect$(arg0: () => void) {
+  throw new Error("Function not implemented.");
+}
 
