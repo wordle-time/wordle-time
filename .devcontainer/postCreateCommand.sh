f86c7064da287707ca8b4712696f253270beb777
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2023-09-15 22:39:37 +0000

    plantuml diagram fix and modern plantuml version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,4 @@ npm install -g prettier
 npm install -g json-server
 npm install
 ./node_modules/.bin/cypress install
+wget https://github.com/plantuml/plantuml/releases/download/v1.2023.11/plantuml-1.2023.11.jar -O plantuml-release.jar
