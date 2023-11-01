428855138fec66d65d188129e1c37170bcd8f4f9
Julian Turner <julian@trner.de>
2023-11-01 20:08:47 +0000

    add pnpm install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
-npm install -g prettier
-npm install -g json-server
-npm install
-./node_modules/.bin/cypress install
+# npm install -g prettier
+# npm install -g json-server
+# npm install
+pnpm install
+#./node_modules/.bin/cypress install
 wget https://github.com/plantuml/plantuml/releases/download/v1.2023.11/plantuml-1.2023.11.jar -O plantuml-release.jar
