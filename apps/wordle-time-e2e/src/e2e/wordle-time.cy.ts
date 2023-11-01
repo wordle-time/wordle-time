cda1d9ff50d3895970167b95e2c61b2ddd3ff09b
Julian Turner <julian@trner.de>
2023-11-01 21:14:29 +0000

    path for screenshots

--- apps/wordle-time-e2e/src/e2e/wordle-time.cy.ts
+++ apps/wordle-time-e2e/src/e2e/wordle-time.cy.ts
@@ -1,19 +1,20 @@
-describe('wordle-time-frontend', () => {
+const group = 'worteingabe-error';
+describe(group, () => {
   beforeEach(() => cy.visit('/'));
 
-  const testname = 'should navigate game';
+  const testname = 'should show too_many_letters';
   it(testname, () => {
     cy.get("a[href='/game']").click();
 
     cy.get('[data-cy="guess-button"]').contains('Raten').should('exist');
 
-    cy.typeWord('aaaaa');
+    // cy.typeWord('aaaaa');
 
-    cy.get('[data-cy="guess-button"]').contains('Raten').click();
+    // cy.get('[data-cy="guess-button"]').contains('Raten').click();
 
-    cy.get('[data-cy="try-count"]').contains('Tries: 1 / 6').should('exist');
+    // cy.get('[data-cy="try-count"]').contains('Tries: 1 / 6').should('exist');
 
-    cy.screenshot('/F001/' + testname, {
+    cy.screenshot('/F001/' + group + ' -- ' + testname, {
       overwrite: true,
     });
   });
