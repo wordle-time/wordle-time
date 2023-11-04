6f7196c81053f711869c3267362e4ee6a7441b05
Julian Turner <tuerner.j@gmail.com>
2023-11-04 11:19:35 +0100

    add text to landing page
    create grid with 3 columns where in the first column is the game name with game elements, in the second the description how the game works and in the last row is the button to navigate to the game

--- apps/wordle-time-frontend/src/routes/index.tsx
+++ apps/wordle-time-frontend/src/routes/index.tsx
@@ -1,43 +1,55 @@
-import { component$, useVisibleTask$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$, useStylesScoped$, useVisibleTask$ } from '@builder.io/qwik';
+import { Link, type DocumentHead } from '@builder.io/qwik-city';
 import { animate, spring } from 'motion';
+import Letter from '../components/letter/letter';
+import { ILetterState } from '@wordle-time/models';
+
 
 export default component$(() => {
-
-  useVisibleTask$(() => {
-    animate(
-      '#h1',
-      // go from 0 to 100 opacity over 1 second
-      // go 20px down
-      {
-        opacity: [0, 1],
-        y: [-20, 0],
-        scale: [0.5, 1],
-      },
-      {
-        duration: 0.3,
-        easing: 'ease-in-out',
-
-      })
-  });
-
-
-
   return (
-    <div>
-      <h1 id='h1' class="text-center pt-6">
-        Welcome wordle-time-frontend <span class="lightning">⚡️</span>
-      </h1>
+    <div class="grid-rows-3">
+      <div class="h-[30vh] text-center flex-row justify-center items-center">
+        <div class="p-4 ">
+          <Letter letterState={"CorrectSpot"} letter='W'  />
+          <Letter letterState={ILetterState.CorrectSpot} letter='O'  />
+          <Letter letterState={ILetterState.CorrectSpot} letter='R'  />
+          <Letter letterState={ILetterState.CorrectSpot} letter='D'  />
+          <Letter letterState={ILetterState.CorrectSpot} letter='L'  />
+          <Letter letterState={ILetterState.CorrectSpot} letter='E'  />
+</div>
+<div class="p-4">
+          <Letter letterState={ILetterState.WrongSpot} letter='T'  />
+          <Letter letterState={ILetterState.WrongSpot} letter='I'  />
+          <Letter letterState={ILetterState.WrongSpot} letter='M'  />
+          <Letter letterState={ILetterState.Undefined} letter='E'  />
+        </div>
+        <h2 class="text-4xl">Can you guess the word of the day?</h2>
+      </div>
+      <div class="h-[50vh] text-center">
+        <h3>The word to guess changes daily, giving players a new challenge every day.</h3>
+        <h4>How to Play?</h4>
+        <p>Letter Input: The player inputs a letter, and the server responds with one of three possible clues:</p>
+        <ul class="list-disc list-inside">
+          <li>The letter is present in the word.</li>
+          <li>The letter is in the correct position in the word.</li>
+          <li>The letter is not in the word at all.</li>
+        </ul>
+        <p>If the player guesses the word within six attempts, they have won the game.</p>
+        <p>If the player fails to guess the word after six attempts, they have lost the game.</p>
+      </div>
+      <div class="p-8 flex items-center justify-center h-[10vh]">
+        <Link class="rounded-lg border-4 p-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base" href='game'>Play now</Link>
+      </div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Wordle Time',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Wordle Time - A wordle clone made with Qwik and Kotlin',
     },
   ],
 };
