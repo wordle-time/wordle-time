a764ee685c1eabf62ab75f2d266c2a18e271c955
Julian Turner <tuerner.j@gmail.com>
2023-11-12 20:52:49 +0100

    refoctor landing

--- libs/ui/src/lib/loading/Loading.tsx
+++ libs/ui/src/lib/loading/Loading.tsx
@@ -1,9 +1,31 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { animate } from "motion";
 
 export default component$(() => {
-    return <>
-        <h2 class="text-3xl loading" data-cy="loading-message">
-            Loading ...
-        </h2>
-    </>
-})
+    useVisibleTask$(() => {
+        const sequence = async () => {
+
+            // Color cycling
+            await animate('.loading', {
+                color: ["#000", "#FF00FF", "#00FFFF", "#FF0000", "#000"]
+            }, { duration: 1 }).finished;
+
+            // Glow effect
+            await animate('.loading', {
+                textShadow: ["0px 0px 2px #000", "0px 0px 10px #00FFFF", "0px 0px 2px #000"]
+            }, { duration: 0.5 }).finished;
+
+            setTimeout(sequence, 2000); // Wait a bit before repeating the sequence
+        };
+
+        sequence();
+    });
+
+    return (
+        <>
+            <h2 class="text-3xl loading" data-cy="loading-message" style="white-space: nowrap;">
+                Loading ...
+            </h2>
+        </>
+    );
+});
