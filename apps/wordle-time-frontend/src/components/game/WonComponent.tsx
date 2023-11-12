3104d25ad4b930bf6826729a6ef0c6ddb219750b
Julian Turner <tuerner.j@gmail.com>
2023-11-12 21:44:41 +0100

    add glow to won component

--- apps/wordle-time-frontend/src/components/game/WonComponent.tsx
+++ apps/wordle-time-frontend/src/components/game/WonComponent.tsx
@@ -1,11 +1,25 @@
-import { component$ } from "@builder.io/qwik"
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { animate } from "motion";
 
 export default component$(() => {
-    return <>
-        <div class="flex items-center justify-center my-16">
-            <h1 class="text-3xl text-ctp-green" data-cy="guess-success">
-                You made it
-            </h1>
-        </div>
-    </>
-})
+    useVisibleTask$(() => {
+        // Pulsating glow effect
+        animate('#guess-success', {
+            textShadow: ['0px 0px 10px #00FF00', '0px 0px 20px #00FF00', '0px 0px 10px #00FF00'],
+            scale: [1, 1.1, 1]
+        }, {
+            duration: 1,
+            repeat: Infinity,
+        });
+    });
+
+    return (
+        <>
+            <div class="flex items-center justify-center my-16 relative">
+                <h1 id="guess-success" class="text-3xl text-ctp-green" data-cy="guess-success">
+                    You made it
+                </h1>
+            </div>
+        </>
+    );
+});
--- libs/ui/src/lib/loading/Loading.tsx
+++ libs/ui/src/lib/loading/Loading.tsx
@@ -1,29 +1,24 @@
 import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { animate } from "motion";
 
+
 export default component$(() => {
     useVisibleTask$(() => {
-        const sequence = async () => {
 
-            // Color cycling
-            await animate('.loading', {
-                color: ["#000", "#FF00FF", "#00FFFF", "#FF0000", "#000"]
-            }, { duration: 1 }).finished;
+        // Color cycling
+        animate('#loading-message', {
+            color: ["#000", "#FF00FF", "#00FFFF", "#FF0000", "#000"]
+        }, { duration: 1 }).finished;
 
-            // Glow effect
-            await animate('.loading', {
-                textShadow: ["0px 0px 2px #000", "0px 0px 10px #00FFFF", "0px 0px 2px #000"]
-            }, { duration: 0.5 }).finished;
-
-            setTimeout(sequence, 2000); // Wait a bit before repeating the sequence
-        };
-
-        sequence();
+        // Glow effect
+        animate('#loading-message', {
+            textShadow: ["0px 0px 2px #000", "0px 0px 10px #00FFFF", "0px 0px 2px #000"]
+        }, { duration: 0.5 }).finished;
     });
 
     return (
         <>
-            <h2 class="text-3xl loading" data-cy="loading-message" style="white-space: nowrap;">
+            <h2 id="loading-message" class="text-3xl loading" data-cy="loading-message" style="white-space: nowrap;">
                 Loading ...
             </h2>
         </>
