db7665070a24a5c7a03938307b2ac37892abbec2
Julian Turner <tuerner.j@gmail.com>
2024-01-12 18:37:38 +0100

    test f002

--- apps/wordle-time-e2e/src/e2e/F002.cy.ts
+++ apps/wordle-time-e2e/src/e2e/F002.cy.ts
@@ -22,9 +22,37 @@ describe(feedback_group, () => {
     cy.get('[data-cy="guess-button"]').click();
     cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-green');
 
-
     cy.screenshot(getScreenshotPath('F002', feedback_group, correct_letter_test), {
       overwrite: true,
     });
   });
+
+  const wrong_spot_test = 'should show wrong_sport';
+  it(wrong_spot_test, () => {
+
+    // if wrong char is entered, guess button should be disabled && input field should be empty
+    cy.get('[data-cy="letter-0"]').clear();
+    cy.get('[data-cy="letter-0"]').type('E');
+    cy.get('[data-cy="guess-button"]').click();
+    cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-yellow');
+
+    cy.screenshot(getScreenshotPath('F002', feedback_group, wrong_spot_test), {
+      overwrite: true,
+    });
+  });
+
+  const wrong_letter_test = 'should show wrong_letter';
+  it(wrong_spot_test, () => {
+
+    // if wrong char is entered, guess button should be disabled && input field should be empty
+    cy.get('[data-cy="letter-0"]').clear();
+    cy.get('[data-cy="letter-0"]').type('U');
+    cy.get('[data-cy="guess-button"]').click();
+    cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-red');
+
+    cy.screenshot(getScreenshotPath('F002', feedback_group, wrong_letter_test), {
+      overwrite: true,
+    });
+  });
+
 });
