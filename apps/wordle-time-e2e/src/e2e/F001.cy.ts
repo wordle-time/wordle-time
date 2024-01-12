f7133071b43b593a1c777a511fb33b86ae8a4318
Julian Turner <tuerner.j@gmail.com>
2024-01-12 16:02:55 +0100

    add f001 tests

---
+++ apps/wordle-time-e2e/src/e2e/F001.cy.ts
@@ -0,0 +1,47 @@
+import { getScreenshotPath } from '../support/utils';
+
+const insert_group = 'worteingabe';
+const error_group = 'worteingabe-error';
+describe(error_group, () => {
+  beforeEach(() => {
+    cy.visit('/game');
+
+
+    // wait for loading screen to disappear and set timeout to 15s
+    cy.get('[data-cy="loading-message"]', { timeout: 15_000 }).should(
+      'not.exist'
+    );
+
+    cy.get('[data-cy="letter-0"]').should('exist');
+  });
+
+  const show_letter_input_test = 'should show letter_input';
+  it(show_letter_input_test, () => {
+
+
+      cy.get('[data-cy="letter-0"]').clear();
+      cy.get('[data-cy="letter-0"]').type('A');
+      cy.get('[data-cy="letter-0"]').should('have.value', 'A');
+
+      cy.wait(500);
+
+      cy.screenshot(getScreenshotPath('F001', insert_group, show_letter_input_test), {
+        overwrite: true,
+      });
+  });
+
+  const show_non_letter_input= 'should show non_letter_input';
+  it(show_non_letter_input, () => {
+
+      cy.get('[data-cy="letter-0"]').clear();
+      cy.get('[data-cy="letter-0"]').type('1');
+      cy.get('[data-cy="letter-0"]').should('have.value', ''); // input field should be empty
+      cy.get('[data-cy="guess-button"]').should('be.disabled'); // guess button should be disabled
+
+      cy.wait(500);
+
+    cy.screenshot(getScreenshotPath('F001', error_group, show_non_letter_input), {
+      overwrite: true,
+    });
+  });
+});
