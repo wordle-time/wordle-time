4b5c059f735ed26765ad31e6cdfa16ec08fb1565
Julian Turner <tuerner.j@gmail.com>
2024-01-17 21:38:15 +0100

    Add remaining time until next word in WonComponent

--- apps/wordle-time-frontend/src/components/game/WonComponent.tsx
+++ apps/wordle-time-frontend/src/components/game/WonComponent.tsx
@@ -1,8 +1,54 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$, $, Resource, useSignal } from "@builder.io/qwik";
 import { animate } from "motion";
 
+const nextGameAtRoute = 'http://127.0.0.1:8090/api/guess/nextWordAt';
+
+export const getNextWordAt = async () => {
+    const response = await fetch(nextGameAtRoute);
+    const result = await response.json();
+    return result as TimeInterface;
+};
+
+interface TimeInterface {
+    time: string;
+}
+
+// return remaining time until next word
+
+
+
 export default component$(() => {
+    const nextGameAt = getNextWordAt();
+    const remainingTime = useSignal("")
+
+
+
     useVisibleTask$(() => {
+        nextGameAt.then((nextGameAtResolved) => {
+            console.log(nextGameAtResolved);
+            setInterval(() => {
+
+                // get difference between now and nextGameAt
+                const now = new Date();
+                const next = new Date(nextGameAtResolved.time);
+                const diff = next.getTime() - now.getTime();
+
+
+                let seconds = Math.floor((diff / 1000) % 60);
+                let minutes = Math.floor((diff / (1000 * 60)) % 60);
+                let hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+                hours = (hours < 10) ? "0" + hours : hours;
+                minutes = (minutes < 10) ? "0" + minutes : minutes;
+                seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+                console.log(hours + ":" + minutes + ":" + seconds);
+
+                remainingTime.value = hours + ":" + minutes + ":" + seconds;
+
+            }, 1000);
+        });
+
         // Pulsating glow effect
         animate('#guess-success', {
             textShadow: ['0px 0px 10px #00FF00', '0px 0px 20px #00FF00', '0px 0px 10px #00FF00'],
@@ -13,10 +59,17 @@ export default component$(() => {
         });
     });
 
+
+
     return (
         <>
-            <div class="flex items-center justify-center my-16 relative">
-                <h1 id="guess-success" class="text-3xl text-ctp-green" data-cy="guess-success">
+            <div id="guess-success" class="flex flex-col items-center justify-center my-16 relative" >
+                {remainingTime.value !== "" &&
+                    <p class="text-3xl text-ctp-green">
+                        Next word in {remainingTime.value}
+                    </p>
+                }
+                <h1 class="text-3xl text-ctp-green" data-cy="guess-success">
                     You made it
                 </h1>
             </div>
