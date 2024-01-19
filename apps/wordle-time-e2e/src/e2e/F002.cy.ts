e23084f68a73c64a00ede2d5f4f295cc01789220
Julian Turner <tuerner.j@gmail.com>
2024-01-19 20:03:20 +0100

    Fix screenshot paths and update test descriptions

--- apps/wordle-time-e2e/src/e2e/F002.cy.ts
+++ apps/wordle-time-e2e/src/e2e/F002.cy.ts
@@ -15,26 +15,33 @@ describe(feedback_group, () => {
 
   const correct_letter_test = 'should show correct_letter';
   it(correct_letter_test, () => {
-
     // if wrong char is entered, guess button should be disabled && input field should be empty
     cy.get('[data-cy="letter-0"]').clear();
     cy.get('[data-cy="letter-0"]').type('A');
     cy.get('[data-cy="guess-button"]').click();
-    cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-green');
+    cy.get('[data-cy="letter-0"]', { timeout: 1_000 }).should(
+      'have.class',
+      'border-ctp-green'
+    );
 
-    cy.screenshot(getScreenshotPath('F002', feedback_group, correct_letter_test), {
-      overwrite: true,
-    });
+    cy.screenshot(
+      getScreenshotPath('F002', feedback_group, correct_letter_test),
+      {
+        overwrite: true,
+      }
+    );
   });
 
-  const wrong_spot_test = 'should show wrong_sport';
+  const wrong_spot_test = 'should show wrong_spot';
   it(wrong_spot_test, () => {
-
     // if wrong char is entered, guess button should be disabled && input field should be empty
     cy.get('[data-cy="letter-0"]').clear();
     cy.get('[data-cy="letter-0"]').type('E');
     cy.get('[data-cy="guess-button"]').click();
-    cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-yellow');
+    cy.get('[data-cy="letter-0"]', { timeout: 1_000 }).should(
+      'have.class',
+      'border-ctp-yellow'
+    );
 
     cy.screenshot(getScreenshotPath('F002', feedback_group, wrong_spot_test), {
       overwrite: true,
@@ -42,17 +49,21 @@ describe(feedback_group, () => {
   });
 
   const wrong_letter_test = 'should show wrong_letter';
-  it(wrong_spot_test, () => {
-
+  it(wrong_letter_test, () => {
     // if wrong char is entered, guess button should be disabled && input field should be empty
     cy.get('[data-cy="letter-0"]').clear();
     cy.get('[data-cy="letter-0"]').type('U');
     cy.get('[data-cy="guess-button"]').click();
-    cy.get('[data-cy="letter-0"]', {timeout: 1_000}).should('have.class', 'border-ctp-red');
+    cy.get('[data-cy="letter-0"]', { timeout: 1_000 }).should(
+      'have.class',
+      'border-ctp-red'
+    );
 
-    cy.screenshot(getScreenshotPath('F002', feedback_group, wrong_letter_test), {
-      overwrite: true,
-    });
+    cy.screenshot(
+      getScreenshotPath('F002', feedback_group, wrong_letter_test),
+      {
+        overwrite: true,
+      }
+    );
   });
-
 });
