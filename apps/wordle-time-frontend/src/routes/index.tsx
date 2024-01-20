ed99b34bed8411ee5c1a1da603f95cdf2483610a
Julian Turner <tuerner.j@gmail.com>
2024-01-20 13:30:38 +0100

    Refactor wordle-time-frontend route component

--- apps/wordle-time-frontend/src/routes/index.tsx
+++ apps/wordle-time-frontend/src/routes/index.tsx
@@ -9,25 +9,23 @@ const getRandomLetterStateString = () => {
 
 export default component$(() => {
   return (
-    <div class="grid-rows-3">
-      <div class="h-[30vh] text-center flex-row justify-center items-center">
-        <h2 class="text-4xl py-10">Can you guess the word of the day?</h2>
-        <div class="p-4 ">
-          <Letter letterState={getRandomLetterStateString()} letter='W' />
-          <Letter letterState={getRandomLetterStateString()} letter='O' />
-          <Letter letterState={getRandomLetterStateString()} letter='R' />
-          <Letter letterState={getRandomLetterStateString()} letter='D' />
-          <Letter letterState={getRandomLetterStateString()} letter='L' />
-          <Letter letterState={getRandomLetterStateString()} letter='E' />
-        </div>
-        <div class="p-4">
-          <Letter letterState={getRandomLetterStateString()} letter='T' />
-          <Letter letterState={getRandomLetterStateString()} letter='I' />
-          <Letter letterState={getRandomLetterStateString()} letter='M' />
-          <Letter letterState={getRandomLetterStateString()} letter='E' />
-        </div>
+    <div class="text-center flex-row justify-center items-center">
+      <h2 class="text-4xl py-10">Can you guess the word of the day?</h2>
+      <div class="p-4 ">
+        <Letter letterState={getRandomLetterStateString()} letter='W' />
+        <Letter letterState={getRandomLetterStateString()} letter='O' />
+        <Letter letterState={getRandomLetterStateString()} letter='R' />
+        <Letter letterState={getRandomLetterStateString()} letter='D' />
+        <Letter letterState={getRandomLetterStateString()} letter='L' />
+        <Letter letterState={getRandomLetterStateString()} letter='E' />
       </div>
-      <div class="h-[50vh] text-center md:pt-40">
+      <div class="p-4">
+        <Letter letterState={getRandomLetterStateString()} letter='T' />
+        <Letter letterState={getRandomLetterStateString()} letter='I' />
+        <Letter letterState={getRandomLetterStateString()} letter='M' />
+        <Letter letterState={getRandomLetterStateString()} letter='E' />
+      </div>
+      <div class="text-center md:pt-40">
         <h3 class="text-2xl " >The word to guess changes daily, giving players a new challenge every day.</h3>
         <h4 class="text-xl py-4">How to Play?</h4>
         <p>The player inputs a letter, and the server responds with one of three possible clues:</p>
@@ -38,11 +36,12 @@ export default component$(() => {
         </ul>
         <p>If the player guesses the word within six attempts, they have won the game.</p>
         <p>If the player fails to guess the word after six attempts, they have lost the game.</p>
-      </div>
-      <div class="p-8 flex items-center justify-center h-[10vh]">
-        <Link class="rounded-lg border-4 p-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base" href='game'>Play now</Link>
+        <div class="p-8 flex items-center justify-center">
+          <Link class="rounded-lg border-4 p-4 border-ctp-blue hover:bg-ctp-blue hover:text-ctp-base" href='game'>Play now</Link>
+        </div>
       </div>
     </div>
+
   );
 });
 
