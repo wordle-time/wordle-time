ea19d8ccaf44c734a400a604bbd6496fdf709f50
Julian Turner <tuerner.j@gmail.com>
2024-01-20 13:30:30 +0100

    Add mobile e2e tests for requirement F006

---
+++ apps/wordle-time-e2e/src/e2e/F006.cy.ts
@@ -0,0 +1,37 @@
+import { getScreenshotPath } from '../support/utils';
+
+const requirement_id = 'F006';
+const mobile_group = 'mobile';
+describe(mobile_group, () => {
+  beforeEach(() => {
+    cy.viewport(412, 915);
+  });
+  const show_rules_test = 'should show rules';
+  it(show_rules_test, () => {
+    cy.visit('/');
+
+    cy.screenshot(
+      getScreenshotPath(requirement_id, mobile_group, show_rules_test),
+      {
+        overwrite: true,
+      }
+    );
+  });
+
+  const show_game_test = 'should show game';
+  it(show_game_test, () => {
+    cy.visit('/game');
+
+    // wait for loading screen to disappear and set timeout to 10s
+    cy.get('[data-cy="loading-message"]', { timeout: 15_000 }).should(
+      'not.exist'
+    );
+
+    cy.screenshot(
+      getScreenshotPath(requirement_id, mobile_group, show_game_test),
+      {
+        overwrite: true,
+      }
+    );
+  });
+});
