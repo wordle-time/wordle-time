7778ba528a01dc6505e041171423f1b50f9bcad8
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2024-01-31 17:44:49 +0100

    print_md_to_pdf.py formatting

--- tools/print_md_to_pdf.py
+++ tools/print_md_to_pdf.py
@@ -1,26 +1,24 @@
 from pathlib import Path
 import subprocess
 
+
 def print_md_to_pdf():
-    def md_to_pdf(md_files_root_Path: Path) -> bool:
+    def md_to_pdf(md_files_root_path: Path):
         """Transforms markdown files into pdf files and deletes markdown files
 
         Args:
-            md_files_root_Path (Path): root directory in which to look for markdown files (recursively)
-
-        Returns:
-            bool: overall success of the operation - operations may fail if pandoc reports problems
+            md_files_root_path (Path): root directory in which to look for markdown files (recursively)
         """
 
-        md_files = sorted(md_files_root_Path.glob('**/*.md'))
+        md_files = sorted(md_files_root_path.glob('**/*.md'))
         if md_files:
             for index, src_str in enumerate(md_files):
                 src_path = Path(src_str)
                 dest_path = src_path.with_suffix(".pdf")
-                print(f"({index+1}/{len(md_files)}): Converting {src_path.relative_to(md_files_root_Path)} -> {dest_path.relative_to(md_files_root_Path)}")
+                print(f"({index+1}/{len(md_files)}): Converting {src_path.relative_to(md_files_root_path)} -> {dest_path.relative_to(md_files_root_path)}")
                 subprocess.run(["/usr/bin/pandoc", src_path, "--from=markdown+rebase_relative_paths", "-o", dest_path])
                 src_path.unlink()
-    
+
     docs_path = Path.cwd().joinpath("docs")
     requirements_output_path = docs_path.joinpath(".requirements")
     other_output_path = docs_path.joinpath("./.other")
